31eaf1c53862d7a5f8037ae83f998b7674871215
Joshua <mramos29@ucol.mx>
2021-09-07 12:43:32 -0500

    Serpientes y Escaleras

--- carrera.js
+++ carrera.js
@@ -14,14 +14,12 @@ class Carrera{
             let casilla = this.serpienteOEscalera(Juan.getPosicion());
             if(casilla !== 0){
                 Juan.setPosicion(casilla)
-                console.log("mocos!");
             }
 
             Roberto.dado();
             casilla = this.serpienteOEscalera(Roberto.getPosicion());
             if(casilla !== 0){
                 Roberto.setPosicion(casilla)
-                console.log("mocos!");
             }
         }
         if(Juan.getPosicion()<100){
@@ -37,56 +35,73 @@ class Carrera{
         switch (casilla){
             //Colocando las escaleras
             case 2:
+                console.log("escalera a 10")
                 return 10;
             break;
             case 7:
+                console.log("escalera a 11")
                 return 11;
             break;
             case 19:
+                console.log("escalera a 21")
                 return 21;
             break;
             case 23:
+                console.log("escalera a 24")
                 return 24;
             break;
             case 44:
+                console.log("escalera a 59")
                 return 59;
             break;
             case 51:
+                console.log("escalera a 53")
                 return 53;
             break;
             case 67:
+                console.log("escalera a 71")
                 return 71;
             break;
             case 76:
+                console.log("escalera a 98")
                 return 98;
             break;
             case 88:
+                console.log("escalera a 90")
                 return 90;
             break;
 
             //colocando serpientes
             case 99:
+                console.log("serpiente a 1")
                 return 1;
             break;
             case 87:
+                console.log("serpiente a 50")
                 return 50;
             break;
             case 70:
+                console.log("serpiente a 60")
                 return 60;
             break;
             case 61:
+                console.log("serpiente a 39")
                 return 39;
             break;
             case 52:
+                console.log("serpiente a 50")
                 return 50;
             break;
             case 40:
-                return 0;
+                console.log("serpiente a 15")
+                return 15;
             break;
             case 17:
+                console.log("serpiente a 16")
                 return 16;
             break;
             case 13:
+                console.log("serpiente a 5")
                 return 5;
             break;
             default:
