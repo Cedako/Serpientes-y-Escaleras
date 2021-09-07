6eeaf683f81d5e7afa622fec7ce9f0f5554b5f6f
Joshua <mramos29@ucol.mx>
2021-09-07 10:17:42 -0500

    bases

---
+++ carrera.js
@@ -0,0 +1,11 @@
+import Ficha from "./ficha.js";
+import Tablero from "./tablero.js";
+
+class Carrera{
+    constructor(){
+        let Juan = new Ficha;
+        let Roberto = new Ficha;
+
+    }
+}
+new Carrera;
---
+++ tablero.js
@@ -0,0 +1,8 @@
+export default class Tablero{
+    constructor(){
+        this._tablero = new Array;
+        for(let i=0;i=100;i++){
+            this._tablero.push(i);
+        }
+    }
+}
